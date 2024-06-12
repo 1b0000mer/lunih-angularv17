32a8eed67188f4e2f271144ad1bbc0b4f8446290
Thien Nguyen <thienquoc98@gmail.com>
2024-06-12 23:44:37 +0300

    add dependencies

--- src/app/content/shared/shared.module.ts
+++ src/app/content/shared/shared.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule, NgStyle } from '@angular/common';
 import { TablePaginateComponent } from './table-paginate/table-paginate.component';
 import { IconModule } from '@coreui/icons-angular';
-import { ContainerComponent, RowComponent, ColComponent, CardGroupComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, CardFooterComponent, FormDirective, InputGroupComponent, InputGroupTextDirective, FormControlDirective, ButtonDirective, SpinnerComponent, FormFeedbackComponent, TableModule, TableDirective, BadgeComponent, PaginationModule, PaginationComponent, PageItemDirective, PageLinkDirective } from '@coreui/angular';
+import { ContainerComponent, RowComponent, ColComponent, CardGroupComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, CardFooterComponent, FormDirective, InputGroupComponent, InputGroupTextDirective, FormControlDirective, ButtonDirective, SpinnerComponent, FormFeedbackComponent, TableModule, TableDirective, BadgeComponent, PaginationModule, PaginationComponent, PageItemDirective, PageLinkDirective, FormModule } from '@coreui/angular';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -12,7 +13,9 @@ import { ContainerComponent, RowComponent, ColComponent, CardGroupComponent, Tex
   ],
   imports: [
     CommonModule,
-
+    FormModule,
+    ReactiveFormsModule,
+    
     ContainerComponent,
     RowComponent,
     ColComponent,
